8441aaf600b7a13e26e29d1dc2bcbe42cb2192f5
Ca10ccgp <caiocesar20080226@gmail.com>
2024-11-26 00:14:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Progamação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programacao',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Oi em Inglês?',
+    'Oi em Inglês é Hi (RAI)'
+)
